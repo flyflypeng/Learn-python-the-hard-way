80a4bde9d47c16cf4c97c1baf8853b0b793cdd26
woshijpf <woshijpfgg@163.com>
2016-10-12 21:54:55 +0800

    add exercise 1-11

---
+++ exercise_5.py
@@ -0,0 +1,14 @@
+my_name = "woshijpf"
+my_age = 24
+my_height = 172.0
+my_weight = 65.0
+my_eyes = "Black"
+my_teeth = "White"
+my_hair = "Black"
+
+print "Let's talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d kg heavy." % my_weight
+print "Actually that's not to heavy.Just fine!"
+print "He's got %s eyes and %s hair." % (my_eyes,my_hair)
+print "His teeth are not very %s !" % my_teeth
